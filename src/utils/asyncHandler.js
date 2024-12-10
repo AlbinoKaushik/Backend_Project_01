cd38ba7d0702c64ac08988d32181e3f37ad1644e
AlbinoKaushik <albinokaushik6618@gmail.com>
2024-12-10 12:34:43 +0530

    make the utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }    
+}
+
+// this is done by promise method
+
+export {asyncHandler}
+
+// this is higher order function
+
+// const asyncHandler = () => {}
+// const asyncHandler = (function) => {() => {}}
+// const asyncHandler = (function) => (() => {})
+// const asyncHandler = (function) => async() => {}
+
+// it is been made by try and catch method
+
+// const asyncHandler = (fn) => async(req,res,next) =>{
+//     try {
+//         await fn(req,res,next)        
+//     } catch (error) {
+//         res.status(err.code  || 500).json({
+//             sucess:false,
+//             message: err.message
+//         })
+        
+//     }
+// }
