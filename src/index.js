f0fa8ba2c5e7e91d33824e1495edf719afee66de
AlbinoKaushik <albinokaushik6618@gmail.com>
2024-12-16 18:46:11 +0530

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config({path: './env'})
+// require('dotenv').config({path: './.env'})
 import dotenv from "dotenv";
  
 // import mongoose, { connect } from "mongoose";
@@ -7,7 +7,7 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 });
 
 
